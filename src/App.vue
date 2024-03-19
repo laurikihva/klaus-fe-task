f09960d3268781f2b37e84892b65225f8ee315b9
Lauri <laurikihva@gmail.com>
2024-03-19 20:41:12 +0200

    add input component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
-  <div class="flex justify-center mt-10">
+  <main class="flex justify-center m-8 mt-10">
     <div class="max-w-[716px] w-full">
-      <div class="flex justify-between items-center mb-5">
+      <div class="flex items-center mb-5">
         <h1 class="text-lg font-medium">Account users</h1>
-        <Button label="Connect users" />
+        <div class="flex gap-2 flex-grow justify-end">
+          <div class="max-w-[205px] w-full">
+            <Input id="search-input" v-model="searchValue" icon="search" placeholder="Search" type="text" />
+          </div>
+          <div class="shrink-0">
+            <Button label="Connect users" />
+          </div>
+        </div>
       </div>
-      <Card> Card content </Card>
+      <Card>
+        <div class="flex items-center gap-6">
+          <h2 class="text-base text-gray-600 font-medium">2 users selected</h2>
+          <div class="flex gap-2">
+            <Button label="Edit" color="secondary" icon="pencil" small />
+            <Button label="Delete" color="secondary" icon="trash" small />
+          </div>
+        </div>
+      </Card>
     </div>
-  </div>
+  </main>
 </template>
 
 <script lang="ts" setup>
-import Button from "@/components/Button.vue";
-import Card from "@/components/Card.vue";
+import { ref } from "vue";
+
+import Button from "./components/Button.vue";
+import Card from "./components/Card.vue";
+import Input from "./components/Form/Input.vue";
+
+const searchValue = ref("");
 </script>
---
+++ src/components/Form/Input.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="flex flex-col gap-2 text-sm">
+    <label v-if="label" :for="id">{{ label }}</label>
+    <div class="relative text-gray-600">
+      <div v-if="icon" class="absolute top-1/2 -translate-y-1/2 left-2 pointer-events-none">
+        <Icon :name="icon" />
+      </div>
+      <input :id v-model="model" :name :class="computedInputClasses" v-bind="attrs" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { toRefs, computed, useAttrs } from "vue";
+
+import Icon, { TIconName } from "../Icon.vue";
+
+export interface IInputProps {
+  id: string;
+  label?: string;
+  name?: string;
+  icon?: TIconName;
+}
+
+const props = defineProps<IInputProps>();
+const { icon } = toRefs(props);
+
+defineOptions({
+  inheritAttrs: false,
+});
+
+const model = defineModel<string | number>();
+
+const attrs = useAttrs();
+
+const computedInputClasses = computed(() => {
+  const base = "w-full min-h-[40px] pr-2 py-2 placeholder:text-gray-100 enabled:focus:outline-blue-400";
+  const withIcon = icon.value ? "pl-8" : "pl-2";
+
+  return [base, withIcon];
+});
+</script>
