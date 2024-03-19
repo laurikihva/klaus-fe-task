7c5d0888868ca30388b717cb33d9843e06bd198a
Lauri <laurikihva@gmail.com>
2024-03-19 22:32:05 +0200

    add avatar component

---
+++ src/components/Avatar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex gap-3 items-center">
+    <div class="w-[32px] h-[32px] rounded-full overflow-hidden">
+      <img :src="image" />
+    </div>
+    <div class="flex flex-col gap-0.5 text-sm">
+      <span class="font-medium">{{ name }}</span>
+      <span class="text-gray-700">{{ email }}</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+export interface IAvatarProps {
+  image: string;
+  name: string;
+  email: string;
+}
+defineProps<IAvatarProps>();
+</script>
